6f2a009bb54f955f8c98ee86c45ddafc3c66c3d3
henrikgabrielsson <hg222aa@student.lnu.se>
2014-07-04 08:45:53 +0000

    place dude

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var canvas = document.getElementById("gameBoard")
-var context = canvas.getContext("2d")
+var mapCanvas = document.getElementById("mapCanvas")
+var mapContext = mapCanvas.getContext("2d")
 
-canvas.width = 500
-canvas.height = 500
+var dudeCanvas = document.getElementById("dudeCanvas")
+var dudeContext = dudeCanvas.getContext("2d")
+
+var canvasHeight = 500
+var canvasWidth = 700
+
+   
+var tilesWidth = 60;
+var tilesHeight = 30;
+
+
+mapCanvas.width = canvasWidth;
+mapCanvas.height = canvasHeight;
+dudeCanvas.height = canvasHeight;
+dudeCanvas.width = canvasWidth;
 
 var tileMap = [
-               [1,1,1,1,1],
-               [1,0,0,0,1],
-               [0,0,0,0,1],
-               [1,0,0,0,1],
-               [1,1,0,1,1]
+               [1,1,1,1,1,1,1,1,1,0,1],
+               [1,0,0,0,1,0,0,0,1,0,1],
+               [0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,1,1,0,1],
+               [1,0,0,0,0,0,0,1,1,0,1],
+               [1,0,0,0,0,0,0,0,0,0,1],
+               [1,1,0,1,1,1,1,1,1,1,1]
                ]
 
 var drawIsoMap = function()
@@ -17,9 +32,7 @@ var drawIsoMap = function()
     
     var tiles = new Image()
     tiles.src = "tiles.png"
-    
-    var tilesWidth = 60;
-    var tilesHeight = 30;
+ 
 
     tiles.addEventListener("load", function(){
         for (var row = 0;row < tileMap.length;row++)
@@ -29,16 +42,32 @@ var drawIsoMap = function()
                 var tileX = tileMap[row][col] * tilesWidth;
                 
                 //x and y position
-                var x = canvas.width/2 + (tilesWidth*col)/2 - (row*tilesWidth)/2
+                var x = mapCanvas.width/2 + (tilesWidth*col)/2 - (row*tilesWidth)/2
                 var y = (row*tilesHeight)/2 + (col*tilesHeight)/2
            
                 //draw tiletype on x and y position
-                context.drawImage(tiles, tileX, 0, tilesWidth, tilesHeight, x, y, tilesWidth, tilesHeight)
+                mapContext.drawImage(tiles, tileX, 0, tilesWidth, tilesHeight, x, y, tilesWidth, tilesHeight)
             }
         }
     }, false)
 
 }
 
-drawIsoMap()
+var drawDude = function()
+{
+    var dudeWidth = 8;
+    var dudeHeight = 30;
+    
+    var dudeX = 30;
+    var dudeY = 0;
+    var dudeIsoX = dudeCanvas.width/2 + tilesWidth/2 + dudeX - dudeY
+    var dudeIsoY = (dudeY + dudeX)/2 - dudeHeight
+    
+    
+    dudeContext.fillStyle ="green"
+    dudeContext.fillRect(dudeIsoX, dudeIsoY, dudeWidth, dudeHeight)
+}
 
+
+    drawIsoMap()
+    drawDude()
